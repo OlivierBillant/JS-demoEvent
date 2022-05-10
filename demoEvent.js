c915119007cd9b03c0b8f005cd7dbbb99d31c9fa
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-10 15:48:48 +0200

    ADD : solution suite

--- demoEvent.js
+++ demoEvent.js
@@ -5,3 +5,14 @@ function compteurIncrement(){
     let paragrapheCompteur = document.getElementById("paragrapheCompteur");
     paragrapheCompteur.innerHTML = "Vous avez cliqué " +compteur +" fois";
 }
+
+function changerCouleur(button){
+    let randomRed = getRandomInt(256);
+    let randomGreen = getRandomInt(256);
+    let randomBlue = getRandomInt(256);
+    button.style.backgroundColor = "RGB("+randomRed+","+randomGreen+","+randomBlue+")";
+}
+
+function getRandomInt(borneMax){
+    return Math.floor(Math.random()*borneMax);
+}
